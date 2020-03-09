006f073309989952e108ef4d792bbe3a832a90fc
Bennett Elder <[omitted]>
2020-03-09 04:37:54 +0000

    Add table vs div option, link target option, classes to tags

--- wp-breezy-hr-openings.php
+++ wp-breezy-hr-openings.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * @package WP_Breezy_HR_Openings
- * @version 0.0.1
+ * @version 0.0.2
  */
 /*
 Plugin Name: WP Breezy HR Openings
-Version: 0.0.1
+Version: 0.0.2
 */
 // Add Shortcode
 function brzyhropenings_shortcode( $atts ) {
@@ -15,6 +15,8 @@ function brzyhropenings_shortcode( $atts ) {
 		array(
             'user' => 'jobs',
             'secondstocache' => '60',
+            'format' => 'table',
+            'linktarget' => '_blank',
 		),
 		$atts
 	);
@@ -22,44 +24,76 @@ function brzyhropenings_shortcode( $atts ) {
     $user = $atts['user'];
     $cache_slug = 'brzyhropenings_' . $user;
     $seconds_to_cache = $atts['secondstocache'];
-    //echo $seconds_to_cache;
-    //$tag = $atts['tag'];
     $url = 'https://' . $user . '.breezy.hr/json';
+    $outputFormat = $atts['format'];
+    $linkTarget = $atts['linktarget'];
 
-    $output = '';
-    $output .= '<p>In theory I would query ' . $url . ' and display those results here.</p>';
+	$output = '';
 
     $openings = get_transient($cache_slug);
 
     if (false === $openings) {
         $response = file_get_contents($url);
         $openings = json_decode($response);
-        echo 'I had to hit the web api';
+        // echo 'I had to hit the web api';
         set_transient($cache_slug, $openings, $seconds_to_cache);
     }
 
+	
     $output .= '<div class="positions-container">';
-    $output .= '<table class="positions"><tbody>';
-    
-    foreach ($openings as $record) {
-        //$output .= '<' . $tag . '>' . $record->name . '</' . $tag . '>';
-        $output .= '<tr class="position">';
-        $output .= '<td class="name"><h2>'
-        . '<a href="' . $record->url . '">' . $record->name . '</a>'
-        . '</h2>'
-        . '<div class="mobile"><div class="type">'
-        . '<span class="label ' . $record->type->id . '"> ' . $record->type->name . ' in </span>'
-        . '<i class="fa fa-map-marker"></i>'
-        . '<span> ' . $record->location->name . '</span>';
 
-        if (isset($record->location->is_remote) && $record->location->is_remote == true) {
-            $output .= ' <i class="fa fa-wifi"></i><span> Remote OK</span>';
+    if ('table' === $outputFormat) {
+	
+        $output .= '<table class="positions"><tbody>';
+        
+        foreach ($openings as $record) {
+            $output .= '<tr class="position"><td class="name">'
+            . '<h2><a href="' . $record->url . '" target="' . $linkTarget . '">' . $record->name . '</a></h2>'
+            . '<div class="mobile"><div class="type">'
+            . '<span class="label ' . $record->type->id . '"> ' . $record->type->name . ' in </span>'
+            . '<span class="location"><i class="fa fa-map-marker"></i> ' . $record->location->name . '</span>';
+
+            if (isset($record->department) && strlen($record->department) > 0) {
+                $output .= ' <span class="department"><i class="fa fa-building"></i> ' . $record->department . '</span>';
+            }
+
+            if (isset($record->location->is_remote) && $record->location->is_remote == true) {
+                $output .= ' <span class="remoteok"><i class="fa fa-wifi"></i> Remote OK</span>';
+            }
+
+            $output .= '</div></div>';
+            $output .= '</td></tr>';
         }
-
-        $output .= '</div></div>' . '</td>' . '</tr>';
+        
+        $output .= '</tbody></table>'; // <table class="positions"><tbody>
     }
+    elseif ('div' === $outputFormat) {
+	
+        $output .= '<div class="positions">';
+        
+        foreach ($openings as $record) {
+            $output .= '<div class="position">';
+            $output .= '<div class="name">'
+            . '<span class="title"><a href="' . $record->url . '" target="' . $linkTarget . '">' . $record->name . '</a></span>'
+            . '<div class="mobile"><div class="type">'
+            . '<span class="label ' . $record->type->id . '"> ' . $record->type->name . ' in </span>'
+            . '<span class="location"><i class="fa fa-map-marker"></i> ' . $record->location->name . '</span>';
 
-    $output .= '</div>';
+            if (isset($record->department) && strlen($record->department) > 0) {
+                $output .= ' <span class="department"><i class="fa fa-building"></i> ' . $record->department . '</span>';
+            }
+
+            if (isset($record->location->is_remote) && $record->location->is_remote == true) {
+                $output .= ' <span class="remoteok"><i class="fa fa-wifi"></i> Remote OK</span>';
+            }
+
+            $output .= '</div></div>';
+            $output .= '</div></div>';
+        }
+        
+        $output .= '</div>'; // class="positions"
+    }
+    $output .= '</div>'; // class="positions-container"
 
     return $output;
 }
